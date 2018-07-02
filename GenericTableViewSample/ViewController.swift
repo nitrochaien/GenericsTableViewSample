2db88799cd599b6dbd104f6958c0eacccb92ac4c
Dinh Vu Nam <vunam0502@gmail.com>
2018-07-02 10:30:50 +0700

    Initial Commit

---
+++ GenericTableViewSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GenericTableViewSample
+//
+//  Created by Dinh Vu Nam on 7/2/18.
+//  Copyright © 2018 Dinh Vu Nam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
